b72a3061df8d3f75d12350f7efa27d0b4253f46c
manelreghima <manelreghima@gmail.com>
2023-05-19 21:39:25 +0100

    update ytsummarizer code

--- ytsummarizer.py
+++ ytsummarizer.py
@@ -9,54 +9,36 @@ import os
 # Set OpenAI API key
 os.environ["OPENAI_API_KEY"] = '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'
 
-# Load documents (or transcribe) with YoutubeLoader
-YT_video = "kv_97RYh1PY&list=RDkv_97RYh1PY&start_radio=1"  # Change this to an empty string or provide a default video ID
-loader = YoutubeLoader(video_id=YT_video, language="en")
-yt_docs = loader.load_and_split()
-embeddings = OpenAIEmbeddings()
-yt_docsearch = Chroma.from_documents(yt_docs, embeddings)
-
-# Define LLM
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.9)
-
-qa_yt = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=yt_docsearch.as_retriever()
-)
-
 # Define Streamlit app
 st.title("Streamlit Chat Demo")
 st.write("This demo uses a chat model to answer questions about a YouTube video.")
 
-    # Display video selection and question inputs
-YT_video = st.text_input("YouTube Video ID (e.g., 1egAKCKPKCk?t=2743)", YT_video)
+# Display video selection and question inputs
+YT_video = st.text_input("YouTube Video ID (e.g., 1egAKCKPKCk?t=2743)")
 query = st.text_input("Enter your question")
 
-    # Process user inputs
+# Process user inputs
 if st.button("Get Answer"):
     if not YT_video:
         st.warning("Please enter a YouTube video ID.")
     elif not query:
         st.warning("Please enter a question.")
     else:
-            # Load documents (or transcribe) with YoutubeLoader
+        # Load documents (or transcribe) with YoutubeLoader
         loader = YoutubeLoader(video_id=YT_video, language="en")
         yt_docs = loader.load_and_split()
         embeddings = OpenAIEmbeddings()
         yt_docsearch = Chroma.from_documents(yt_docs, embeddings)
 
-            # Define LLM
+        # Define LLM
         llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.9)
 
         qa_yt = RetrievalQA.from_chain_type(
             llm=llm,
             chain_type="stuff",
             retriever=yt_docsearch.as_retriever()
-            )
+        )
 
-            # Get answer
+        # Get answer
         result = qa_yt.run(query)
         st.write("Answer:", result.answer)
-
-
